531a2db1e89591b41aa4a51ff57bb5571b321982
Adinath-S-Pawar <21aspawar16055@gmail.com>
2026-07-03 20:15:26 +0530

    brute xn

--- Array part3/powxn.cpp
+++ Array part3/powxn.cpp
@@ -33,3 +33,25 @@ double myPow(double x, int n) {
 
     return result;
 }
+
+//brute
+double myPow(double x, int n) {
+        // Base case: any number to the power of 0 is 1
+        if (n == 0 || x == 1.0) return 1; 
+        
+        long long temp = n; // to avoid integer overflow
+        
+        // Handle negative exponents
+        if (n < 0) { 
+            x = 1 / x;
+            temp = -1*1LL*n;
+        }
+
+        double ans = 1;
+
+        for (long long i = 0; i < temp; i++) {
+            // Multiply ans by x for n times
+            ans *= x; 
+        }
+        return ans;
+    }
